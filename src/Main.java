e03647d5f0cb0640f5013b087057285a61031099
Jeferson Nascimento <mjef013@gmail.com>
2025-05-05 06:54:25 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse
+           usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner sc = new Scanner(System.in);
+
+        double salaryBase = 1293.20;
+        double salary = sc.nextDouble();
+
+        double qtdSalaryBase = Math.ceil((salary / salaryBase) * 10) / 10.0;
+
+        System.out.println("A quantidade de salarios minimos : " + qtdSalaryBase );
+
+
+
+
+        sc.close();
     }
 }
